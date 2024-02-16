e18c418a2c9fe6eebbb9d9e73ea8aa1d1a7d53b3
Ese Amadasun <amadasunese@gmail.com>
2024-02-16 12:39:55 +0100

    updated config

--- application/config.py
+++ application/config.py
@@ -1,27 +1,27 @@
-from decouple import config
-from app import app
+# from decouple import config
+# from app import app
 
-# Configure Flask-Mail
-# MAIL_SERVER = "smtp.gmail.com"
-# MAIL_PORT = 465
-# MAIL_USE_SSL = True
-# MAIL_USERNAME = config("EMAIL_USER")
-# MAIL_PASSWORD = config("EMAIL_PASSWORD")
-# MAIL_DEFAULT_SENDER = 'amadasunese@gmail.com'
-
-
-# Configure Flask-Mail
-app.config["MAIL_SERVER"] = "smtp.gmail.com"
-app.config["MAIL_PORT"] = 465
-app.config["MAIL_USE_SSL"] = True
-app.config["MAIL_USERNAME"] = 'amadasunese@gmail.com'
-app.config["MAIL_PASSWORD"] = 'qxxo axga dzia jjsw'
-app.config['MAIL_DEFAULT_SENDER'] = 'amadasunese@gmail.com'
+# # Configure Flask-Mail
+# # MAIL_SERVER = "smtp.gmail.com"
+# # MAIL_PORT = 465
+# # MAIL_USE_SSL = True
+# # MAIL_USERNAME = config("EMAIL_USER")
+# # MAIL_PASSWORD = config("EMAIL_PASSWORD")
+# # MAIL_DEFAULT_SENDER = 'amadasunese@gmail.com'
 
 
+# # Configure Flask-Mail
+# app.config["MAIL_SERVER"] = "smtp.gmail.com"
+# app.config["MAIL_PORT"] = 465
+# app.config["MAIL_USE_SSL"] = True
+# app.config["MAIL_USERNAME"] = 'amadasunese@gmail.com'
+# app.config["MAIL_PASSWORD"] = 'qxxo axga dzia jjsw'
+# app.config['MAIL_DEFAULT_SENDER'] = 'amadasunese@gmail.com'
 
 
-SECRET_KEY ='a47680abc56efeea392f4af3a03453ee'
+
+
+# SECRET_KEY ='a47680abc56efeea392f4af3a03453ee'
 
 
 
